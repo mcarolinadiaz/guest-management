8417038b4b06505b27d46d338eaee144fae82546
mcarolinadiaz <diazmariacarolina6@gmail.com>
2024-08-24 10:34:06 -0300

    Se genera y contruye componente de edicion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ConfirmedGuestsComponent } from './confirmed-guests/confirmed-guests.component';
 import { ListGuestComponent } from './list-guest/list-guest.component';
+import { EditGuestComponent } from './home/edit-guest/edit-guest.component';
 /*
 export const routes: Routes = [
     { path: '', component: HomeComponent},
@@ -16,7 +17,8 @@ export const routes: Routes = [
       children: [
         { path: '', redirectTo: 'guests', pathMatch: 'full' }, // ruta por defecto al cargar HomeComponent
         { path: 'confirmed-guests', component: ConfirmedGuestsComponent },
-        { path: 'guests', component: ListGuestComponent }
+        { path: 'guests', component: ListGuestComponent },
+        { path: 'edit-guest', component: EditGuestComponent}
       ]
     },
     { path: '**', redirectTo: '', pathMatch: 'full' }
